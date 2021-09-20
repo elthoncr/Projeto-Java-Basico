3c397a7786ce60300a5e998af8256fbe3ee317e3
Elthon Rodrigues <elthon.c.r@gmail.com>
2021-09-20 20:41:03 -0300

    Esse projeto exemplifica o uso de objetos

---
+++ src/objetos/Classes.java
@@ -0,0 +1,14 @@
+package objetos;
+
+public class Classes {
+
+	public static void main(String[] args) {
+		
+		Pessoa eu = new Pessoa();
+		eu.nome = "José";
+		
+		System.out.println(eu.nome);
+
+	}
+
+}
---
+++ src/objetos/Pessoa.java
@@ -0,0 +1,7 @@
+package objetos;
+
+public class Pessoa {
+
+	String nome = "Elthon";
+	
+}
